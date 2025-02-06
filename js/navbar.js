4e95dad4ba20a915c30e19b3cf77797a45018245
Torii Barnard <tor.barnard@gmail.com>
2025-02-05 21:21:09 -0400

    basic styling and navbar

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading navbar:", error));
-});
